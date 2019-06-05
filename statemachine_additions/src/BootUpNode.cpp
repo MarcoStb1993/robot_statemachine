bdb96420c955bdc9c2eeb6c59b8fc6c34f54e6f9
MarcoStb1993 <marco.steinbrink@outlook.de>
2019-06-05 13:01:16 +0200

    Removed ROSINFO from BootUpNode

--- statemachine_additions/src/BootUpNode.cpp
+++ statemachine_additions/src/BootUpNode.cpp
@@ -4,37 +4,33 @@
 bool boot_finished = false;
 bool node_finished = false;
 
-bool bootUpService(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
-{
-    ROS_INFO("Bootup finished request received");
-    if(boot_finished)
-    {
-        res.success = 1;
-        res.message = "Finished";
-        node_finished = true;
-    }
-    else
-    {
-        res.success = false;
-        res.message = "Still booting ...";
-    }    
-    return true;
+bool bootUpService(std_srvs::SetBool::Request &req,
+		std_srvs::SetBool::Response &res) {
+	if (boot_finished) {
+		res.success = 1;
+		res.message = "Finished";
+		node_finished = true;
+	} else {
+		res.success = false;
+		res.message = "Still booting ...";
+	}
+	return true;
 }
 
-void timerCallback(const ros::TimerEvent&)
-{
-    boot_finished = true;
+void timerCallback(const ros::TimerEvent&) {
+	boot_finished = true;
+	system("rostopic info /cmd_vel");
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "bootUpNode");
-    ros::NodeHandle nh("statemachine");
-    ros::ServiceServer bootup_service = nh.advertiseService("bootUpFinished", bootUpService);
-    ros::Timer timer = nh.createTimer(ros::Duration(1), timerCallback, true);
-    while(ros::ok() && !node_finished)
-    {
-        ros::spin();
-    }
-    return 0;
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "bootUpNode");
+	ros::NodeHandle nh("statemachine");
+	ros::ServiceServer bootup_service = nh.advertiseService("bootUpFinished",
+			bootUpService);
+	ros::Timer timer = nh.createTimer(ros::Duration(0.5), timerCallback, true);
+
+	while (ros::ok() && !node_finished) {
+		ros::spin();
+	}
+	return 0;
 }
